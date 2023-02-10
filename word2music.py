1c4f7d3ac30209291e51b660ca38b216f37f9a0b
konju-2 <124993518+konju-2@users.noreply.github.com>
2023-02-10 18:18:39 +0900

    Update word2music.py

--- word2music.py
+++ word2music.py
@@ -33,7 +33,7 @@ class SentenceBertJapanese:
 
 bertModel = SentenceBertJapanese("bertModel")
 
-from tensorflow.keras.models import load_model
+from keras.models import load_model
 import numpy as np
 
 musics = [
